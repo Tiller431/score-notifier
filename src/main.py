4d685012b5a65cbf6ab8641f8cd16162f87ed673
BuildTools <unconfigured@null.spigotmc.org>
2021-09-27 19:10:35 -0500

    update

--- src/main.py
+++ src/main.py
@@ -7,26 +7,28 @@ import logging as log
 
 
 # Config
-lbwh = "https://discord.com/api/webhooks/890431880124563527/nJPDD0Si0qhstTN_vx1Oq36qI0d4PPqFtXUvl-s9o9n80dIpfj1uDsKEvXSWif35C76Y"
-ppwh = "https://discord.com/api/webhooks/890472004002664448/T58YyTYxJMijUOyNTdxUxVh4i5fX8a0h9SUvmnyotJPWXxnEuDZGVwtndEUjacL5KTNg"
+scoresUrl = "https://discord.com/api/webhooks/890472004002664448/T58YyTYxJMijUOyNTdxUxVh4i5fX8a0h9SUvmnyotJPWXxnEuDZGVwtndEUjacL5KTNg"
 log.basicConfig(level=log.INFO)
 url = "http://127.0.0.1:24050/json"
 
+# Check for updates
+releaseURL = "https://api.github.com/repos/Tiller431/score-notifier/actions/artifacts"
 
-def sendMSG(msg, url):
+resp = requests.get(releaseURL).json()
+latestVersion = resp["artifacts"][0]
+print("Download for the newest release of this program:", latestVersion["archive_download_url"])
+
+
+def sendMSG(msg):
     data = {
         "content": msg,
     }
-    requests.post(url, json=data)
+    requests.post(scoresUrl, json=data)
 
 
 def startGosu():
-    # Change this to the path of your exe if this isnt in the same directory
+    # Change this to the path of your exe if this isnt in the same directory example: C:\\GosuMemory\\gosumemory.exe
     os.system("gosumemory.exe")
-
-
-#def checkUpdates():
-    # https://api.github.com/repos/octocat/hello-world/releases/latest
 
 
 try:
@@ -43,10 +45,20 @@ while True:
     if playerData["menu"]["state"] == oldState:
         continue
 
-    if playerData["menu"]["state"] == 7:
-        oldState = 7
-        log.info("Player is on score screen")
+    if playerData["menu"]["state"] == 2:
+        oldState = 2
+        log.info("Player is playing a map")
 
-    if playerData["menu"]["state"] == 5:
-        oldState = 5
-        log.info("Player is selecting a map")
+    if playerData["menu"]["state"] == 7 and oldState == 2:
+        oldState = 7
+        log.info("Player just set a score. Sending to discord!")
+        msg = "New play from {}!\n".format(playerData["resultsScreen"]["name"])
+        
+        msg += "▸ {} ▸ {}PP ({}PP for {}% FC) ▸ {}%\n".format(playerData["gameplay"]["hits"]["grade"]["current"], playerData["gameplay"]["pp"]["current"], playerData["gameplay"]["pp"]["fc"], playerData["gameplay"]["accuracy"], playerData["gameplay"]["accuracy"])
+        msg += "▸ {} ▸ x{}/{} ▸ [{}/{}/{}/{}]\n".format(playerData["gameplay"]["score"], playerData["gameplay"]["combo"]["current"], playerData["gameplay"]["combo"]["max"], playerData["gameplay"]["hits"]["300"], playerData["gameplay"]["hits"]["100"], playerData["gameplay"]["hits"]["50"], playerData["gameplay"]["hits"]["0"])
+
+        sendMSG(msg)
+    elif playerData["menu"]["state"] == 7 and oldState != 2:
+        oldState = 7
+        log.info("Player is looking at a score")
+
